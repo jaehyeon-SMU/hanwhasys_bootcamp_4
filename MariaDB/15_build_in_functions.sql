db7a472042d916db33b7d282c9f85bf6bdb521bb
cookeeholic <cookeeholic@naver.com>
2023-12-28 10:56:42 +0900

    mariaDb sql
    Build in function  학습

--- MariaDB/15_build_in_functions.sql
+++ MariaDB/15_build_in_functions.sql
@@ -48,5 +48,107 @@ SELECT
 		
 		
 SELECT CONCAT(menu_name, '의 가격은', menu_price, '입니다.') FROM tbl_menu;
+
+-- FORMAT(숫자, 소수점 자릿수)
+SELECT FORMAT(12312313213, 3);
+
+-- BIN(숫자), OCT(숫자), HEX(숫자)
+SELECT BIN(65), OCT(65), HEX(65);
+
+-- REPEAT(문자열, 횟수)
+SELECT REPEAT('오져따리 ',5);
+
+-- REPLACE(문자열, 찾을 문자열, 바꿀 문자열)
+SELECT REPLACE('마리아DB', '마리아', 'Maria');
+
+-- REVERSE(문자열)
+SELECT REVERSE('HAPPINESS');
+
+-- SPACE(길이)
+SELECT CONCAT('제 포켓몬은', SPACE(3) , '이고, 속성은', SPACE(6), '입니다.');
+
+-- ★SUBSTRING(문자열, 시작위치, 길이)
+SELECT SUBSTRING('열심히 DB공부를 해 봐요!', 5 , 4);
+SELECT SUBSTRING('열심히 DB공부를 해!', 9); -- 9번째 문자부터 끝까지
+
+-- SUBSTRING_INDEX(문자열, 구분자, 횟수)
+SELECT
+		  SUBSTRING_INDEX('010-1234-5678', '-', 2)
+		, SUBSTRING_INDEX('010-1234-5678', '-', -1);
+
+
 -- 2. 숫자 관련 함수
+-- ABS(숫자) 절댓값
+SELECT ABS(-123);
+
+-- CEILING(숫자)올림, FLOOR(숫자)버림, ROUND(숫자)반올림
+SELECT CEILING(1234.56), FLOOR(1234.56), ROUND(1234.56);
+
+-- CONV(숫자, 원래 진수, 변환할 진수)
+SELECT CONV('A', 16,10) ,  CONV('A',16,2);
+
+-- MOD(숫자1, 숫자2) 나머지
+SELECT MOD(10 , 3);
+
+-- POW(숫자1, 숫자2) 제곱, SQRT(숫자) 루트
+SELECT POW(3,2), SQRT(81);
+
+-- RAND()
+-- RAND() * 생성할 난수 갯수 + 난수의 초기값
+-- EX) 5~33 난수 생성일 경우 RAND()*29(33 - 5 +1) +5 
+SELECT FLOOR(RAND() * 4 +5);
+
+-- sign(숫자)
+SELECT SIGN(10.1), SIGN(0), SIGN(-1,1);
+
+-- TRUNCATE(숫자,정수)
+SELECT TRUNCATE(12345.12345,2), TRUNCATE(12345.12345, -2);
+
+
 -- 3. 날짜 및 시간 관련 함수
+-- ADDDATE(날짜, 차이), SUBDATE(날짜, 차이)
+SELECT ADDDATE('2020-02-01', INTERVAL 28 DAY), ADDDATE('2020-02-01', 28);
+SELECT SUBDATE('2020-02-01', INTERVAL 1 DAY), SUBDATE('2020-02-01', 1);
+
+SELECT ADDDATE('2020-02-01', INTERVAL 28 MONTH);
+SELECT SUBDATE('2020-02-01', INTERVAL 1 MONTH);
+
+-- ADDTIME(날짜/시간, 시간), SUBTIME(날짜/시간, 시간)
+SELECT ADDTIME('2023-12-28 10:27:00', '1:0:10');
+SELECT SUBTIME('2023-12-28 10:27:00', '1:0:10');
+
+-- CURDATE() , CURTIME(), NOW(), SYSDATE()
+SELECT CURDATE(), CURTIME(), NOW(), SYSDATE();
+
+SELECT CURTIME(), CURRENT_TIME(), CURRENT_TIME;
+
+-- YEAR(날짜), MONTH(날짜), DAY(날짜)
+-- DATE(날짜/시간), TIME(날짜/시간)
+SELECT YEAR(CURDATE()), MONTH(CURDATE()), DAY(CURDATE()), DAY(CAST('2023-12-28' AS DATE));
+SELECT HOUR(CURTIME()), MINUTE(CURTIME()), SECOND(CURTIME());
+
+SELECT DATE(NOW()), TIME(NOW());
+
+-- DATEDIFF(날짜1, 날짜2), TIMEDIFF(날짜1 또는 시간1, 날짜2 또는 시간2)
+SELECT DATEDIFF('2024-06-14','2023-12-28'), TIMEDIFF('18:00:00',CURTIME());
+
+-- DAYOFWEEK(날짜), MONTHNAME(날짜) DAYOFYEAR(날짜)
+SELECT DAYOFWEEK(CURDATE()), MONTHNAME(CURDATE()), DAYOFYEAR(CURDATE());
+
+-- LAST_DAY(날짜)
+SELECT LAST_DAY('20230201');
+
+-- MAKEDATE(연도, 지난 날)
+SELECT MAKEDATE(2023, 35);
+
+-- MAKETIME(시간, 분, 초)
+SELECT MAKETIME(17,50,01);
+
+-- PERIOD_ADD(연월, 개월 수), PERIOD_DIFF(연월1, 연월2)
+SELECT PERIOD_ADD(CURDATE(), 7);
+
+-- QUARTER(날짜)
+SELECT QUARTER('2023-12-28');
+
+--TIME_TO_SEC(시간)
+SELECT TIME_TO_SEC(CURTIME());
