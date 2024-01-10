4fe0be63c5347cd5336c9c27140681b291fb0595
cookeeholic <cookeeholic@naver.com>
2024-01-10 12:00:05 +0900

    java 출력문,계산

---
+++ Java/java/com/ohgiraffers/section01/literal/Application2.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application2 {
+    public static void main(String[] args) {
+        /* 수업목표. 값을 직접 연산하여 출력할 수 있다.*/
+
+        // 목차. 1. 숫자와 숫자의 연산
+        System.out.println("====================== 정수와 정수의 연산 =================");
+        System.out.println(123 + 456);
+        System.out.println(123 - 23);
+        System.out.println(123 * 10);
+        System.out.println(123 / 10);
+        System.out.println(123 % 10);
+
+        // 목차. 2. 문자의 연산
+        System.out.println("====================== 실수와 실수의 연산 =================");
+        System.out.println(1.23 + 1.23);
+        System.out.println(1.23 - 0.23);
+        System.out.println(1.23 * 10.0);
+        System.out.println(1.23 / 10.0);
+        System.out.println(1.23 % 1.0);
+
+        // 목차. 3. 문자열의 연산
+
+        // 목차. 4. 논리값의 연산
+    }
+}
