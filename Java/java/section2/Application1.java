fba6af0c217721df0663ec90cd39914a3fb1ba07
cookeeholic <cookeeholic@naver.com>
2024-01-10 12:49:13 +0900

    Java 변수

---
+++ Java/java/section2/Application1.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.section02.variavle;
+
+public class Application1 {
+    public static void main(String[] args) {
+        int num;
+        num = 1;
+
+        System.out.println("num = " + num);
+    }
+}
