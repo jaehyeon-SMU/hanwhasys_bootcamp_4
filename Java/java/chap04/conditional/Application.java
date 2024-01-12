01f083ca462194481afe63d91aece9f9857abf6a
cookeeholic <cookeeholic@naver.com>
2024-01-12 16:06:35 +0900

    Java conditional(if문, switch-case문) 학습

--- Java/java/chap04/conditional/Application.java
+++ Java/java/chap04/conditional/Application.java
@@ -25,7 +25,12 @@ public class Application {
         /* 필기. 단독 if-else-if문 흐름 확인용 메소드 호출 */
         //cClass.testSimpleIfElseIfStatement();
         /* 필기. 중첩 if-else-if문 흐름 확인용 메소드 호출 */
-        cClass.testNestedIfElseIfStatement();
+        //cClass.testNestedIfElseIfStatement();
         /* 필기. 중첩 if-else-if문 로직 개선 */
+
+        D_switch dClass = new D_switch();
+
+        /* 필기. switch문 흐름 확인용 메소드 호출 */
+        dClass.testSimpleSwitchStatement();
     }
 }
