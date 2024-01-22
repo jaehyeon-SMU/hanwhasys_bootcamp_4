c8ad688c000a8e5446bed5c3ee26691fbf1c02be
cookeeholic <cookeeholic@naver.com>
2024-01-22 16:27:04 +0900

    Java Generic< 제네릭, extends, super >학습

---
+++ Java/java/chap10/extend/vo/DrunkenBunny.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.section02.extend.vo;
+
+public class DrunkenBunny extends Rabbit{
+
+    @Override
+    public void cry(){
+        System.out.println("보니하니 내이름은 이한위 당근 칵퉤");
+    }
+}
+
