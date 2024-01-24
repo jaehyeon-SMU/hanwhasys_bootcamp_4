610a917d978f76cd06a736545199958e7878a7cc
cookeeholic <cookeeholic@naver.com>
2024-01-24 11:14:26 +0900

    Java userException(사용자 정의 예외발생 구문) 학습

---
+++ Java/java/chap12/userexception/exception/NotEnoughMonetException.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.section02.userexception.exception;
+
+public class NotEnoughMonetException extends Exception {
+    public NotEnoughMonetException(String message) {
+        super(message);
+    }
+}
