53358e00666d619936535fc497638ec320f8eea9
cookeeholic <cookeeholic@naver.com>
2024-02-01 15:30:16 +0900

    Java dp(다이나믹 프로그래밍 문제 풀이)

---
+++ Java/java/chap17/dp/main/dp/Application2.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.chap06.section01.dp;
+
+
+import java.io.*;
+
+public class Application2 {
+    public static BufferedReader toBufferedReader(String str) {
+        InputStream is = new ByteArrayInputStream(str.getBytes());
+        return new BufferedReader(new InputStreamReader(is));
+    }
+
+    public static int solution(int n){
+        int[] dp = new int[1000 + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]);
+        }
+
+        return dp[n];
+    }
+}
