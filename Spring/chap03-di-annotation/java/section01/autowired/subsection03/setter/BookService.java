b1d86fa25f442ac1bdfe9e36c5789ac4f0cd39b4
cookeeholic <cookeeholic@naver.com>
2024-02-13 15:50:36 +0900

    Spring di-annotation autowired 학습

---
+++ Spring/chap03-di-annotation/java/section01/autowired/subsection03/setter/BookService.java
@@ -0,0 +1,29 @@
+package com.ohgiraffers.section01.autowired.subsection03.setter;
+
+import com.ohgiraffers.section01.common.BookDAO;
+import com.ohgiraffers.section01.common.BookDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("bookServiceSetter")
+public class BookService {
+
+    private BookDAO bookDAO;
+
+    @Autowired
+    public void setBookDAO(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    /* 설명. 도서 목록 전체 조회 */
+    public List<BookDTO> findAllBook(){
+        return bookDAO.findAllBook();
+    }
+
+    /* 설명. 도서 번호로 도서 조회 */
+    public BookDTO searchBookBySequence(int sequence) {
+        return bookDAO.searchBookBySequence(sequence);
+    }
+}
