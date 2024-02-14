573169e2236fadfebda3771753ee06377dcf8363
cookeeholic <cookeeholic@naver.com>
2024-02-14 12:46:36 +0900

    Spring bean-scope initdestroy, properties

---
+++ Spring/chap04-bean-scope/java/section02/initdestroy/Owner.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section02.initdestroy;
+
+public class Owner {
+
+    public void openShop(){
+        System.out.println("사장님이 가게 문을 열었습니다. 이제 쇼핑하세욧!!");
+    }
+
+    public  void closeShop(){
+        System.out.println("사장님이 가게 문을 닫았습니다. 집에 곱게 들어가세욧!!");
+    }
+}
