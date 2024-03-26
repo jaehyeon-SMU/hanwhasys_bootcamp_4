8e8cdecf8df5ac9b6d0b8413b6db540d8e7cdf3a
cookeeholic <cookeeholic@naver.com>
2024-03-26 15:59:06 +0900

    Vue props 학습

---
+++ Vue/07_props/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import PropsParent from "./components/01_propsBasic/PropsParents.vue"
+  import DrillingParents from "./components/01_propsDrilling/DrillingParents.vue"
+</script>
+
+<template>
+  <header>
+    <PropsParent/>
+  </header>
+
+  <main>
+    <DrillingParents/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
